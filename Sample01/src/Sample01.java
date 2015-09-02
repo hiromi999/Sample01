d039bd41041c3906833b272cfd76697e7fa2d663
Hiromi <Hiromi@Dragon>
2015-09-02 22:20:48 +0900

    追加修正１

--- Sample01/src/Sample01.java
+++ Sample01/src/Sample01.java
@@ -3,9 +3,10 @@ public class Sample01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		//Git Test 2015/09/02 16:55
 
 		//Add Test 2015/09/02 18:45
+		System.out.println("welcome to Git");
+		System.out.println("追加修正１");
 
 	}
 
