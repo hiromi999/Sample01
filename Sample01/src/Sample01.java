0750b951bf10c9494fe617f321060c0bd97fc98a
hiromi <takahi@chiba-fjb.ac.jp>
2015-09-27 16:55:47 +0900

    update

--- Sample01/src/Sample01.java
+++ Sample01/src/Sample01.java
@@ -4,7 +4,7 @@ public class Sample01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		//Git Test 2015/09/02 16:55
-
+		//Add Test 2015/09/02 16:58
 		//Add Test 2015/09/02 18:45
 
 	}
